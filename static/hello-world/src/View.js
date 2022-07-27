31f408f0c7e1d4de23b248b7c74b3e039db8f519
Fırat Doğan <firatdogan0@gmail.com>
2022-07-27 14:07:50 +0300

    first commit

---
+++ static/hello-world/src/View.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { view, invoke } from "@forge/bridge";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+
+const data01 = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+const data02 = [
+  { name: "A1", value: 100 },
+  { name: "A2", value: 300 },
+  { name: "B1", value: 100 },
+  { name: "B2", value: 80 },
+  { name: "B3", value: 40 },
+  { name: "B4", value: 30 },
+  { name: "B5", value: 50 },
+  { name: "C1", value: 100 },
+  { name: "C2", value: 200 },
+  { name: "D1", value: 150 },
+  { name: "D2", value: 50 },
+];
+
+function View() {
+  const [context, setContext] = useState();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    invoke("getText", { example: "my-invoke-variable" }).then(setData);
+  }, []);
+
+  useEffect(() => {
+    view.getContext().then(setContext);
+  }, []);
+
+  if (!context || !data) {
+    return "Loading...";
+  }
+
+  return (
+    <div>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data01}
+          dataKey="value"
+          cx="50%"
+          cy="50%"
+          outerRadius={60}
+          fill="#8884d8"
+        />
+        <Pie
+          data={data02}
+          dataKey="value"
+          cx="50%"
+          cy="50%"
+          innerRadius={70}
+          outerRadius={90}
+          fill="#82ca9d"
+          label
+        />
+      </PieChart>
+    </div>
+  );
+}
+
+export default View;
